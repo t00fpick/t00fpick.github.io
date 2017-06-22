65b459d98b1ec98e23026e7d82c0a97c7f4f1c9b
Nathan <toofpick2@hotmail.com>
2017-06-22 13:13:20 -0600

    Gallery v2.0

--- pages/gallery/java.js
+++ pages/gallery/java.js
@@ -1,42 +1,27 @@
-
-
-
-function myFunction() {
-	var x = document.getElementById("myTopnav");
-	if (x.className === "topnav") {
-		x.className += " responsive";
-	} else {
-		x.className = "topnav";
-	}
-}
-
-//side-nav-bar//
-
-$(document).ready(function(){
-
-	$('div.dropdown').each(function() {
-		var $dropdown = $(this);
-
-		$("a.dropdown-link", $dropdown).click(function(e) {
-			e.preventDefault();
-			$div = $("div.dropdown-container", $dropdown);
-			$div.toggle();
-			$("div.dropdown-container").not($div).hide();
-			return false;
-		});
-	});
-
-
-myShow = w3.slideshow(".maincontent", 0);
-musicShow = w3.slideshow(".music", 0);
-
-
-
+$(".link").click(function(e) {
+      e.preventDefault();
+      $('.content-container div').fadeOut('slow');
+      $('#' + $(this).data('rel')).fadeIn('slow');
 });
 
 
+var slideIndex = 1;
+showDivs(slideIndex);
 
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
 
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1} 
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none"; 
+    }
+    x[slideIndex-1].style.display = "block"; 
+}
 
 
 
